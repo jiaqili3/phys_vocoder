10d3a06ceb214e0fe2fd1ac99d48c7d758509853
Jiaqi Li <120090727@link.cuhk.edu.cn>
2023-06-13 18:33:47 +0800

    pgd

--- attack/attacks/pgd.py
+++ attack/attacks/pgd.py
@@ -19,13 +19,13 @@ class PGD(Attack):
         random_start (bool): using random initialization of delta. (Default: True)
 
     Shape:
-        - images: :math:`(N, C, H, W)` where `N = number of batches`, `C = number of channels`,        `H = height` and `W = width`. It must have a range [0, 1].
-        - labels: :math:`(N)` where each value :math:`y_i` is :math:`0 \leq y_i \leq` `number of labels`.
+        - x2: :math:`(N, C, H, W)` where `N = number of batches`, `C = number of channels`,        `H = height` and `W = width`. It must have a range [0, 1].
+        - y: :math:`(N)` where each value :math:`y_i` is :math:`0 \leq y_i \leq` `number of y`.
         - output: :math:`(N, C, H, W)`.
 
     Examples::
         >>> attack = torchattacks.PGD(model, eps=8/255, alpha=1/255, steps=10, random_start=True)
-        >>> adv_images = attack(images, labels)
+        >>> adv_x2 = attack(x2, y)
 
     """
 
@@ -37,43 +37,44 @@ class PGD(Attack):
         self.random_start = random_start
         self.supported_mode = ['default', 'targeted']
 
-    def forward(self, images, labels):
+    def forward(self, x1, x2, y):
         r"""
         Overridden.
         """
 
-        images = images.clone().detach().to(self.device)
-        labels = labels.clone().detach().to(self.device)
+        x1 = x1.clone().detach().to(self.device)
+        x2 = x2.clone().detach().to(self.device)
+        y = y.clone().detach().to(self.device)
 
         if self.targeted:
-            target_labels = self.get_target_label(images, labels)
+            target_y = self.get_target_label(x2, y)
 
         loss = nn.CrossEntropyLoss()
-        adv_images = images.clone().detach()
+        adv_x2 = x2.clone().detach()
 
         if self.random_start:
             # Starting at a uniformly random point
-            adv_images = adv_images + \
-                torch.empty_like(adv_images).uniform_(-self.eps, self.eps)
-            adv_images = torch.clamp(adv_images, min=0, max=1).detach()
+            adv_x2 = adv_x2 + \
+                torch.empty_like(adv_x2).uniform_(-self.eps, self.eps)
+            adv_x2 = torch.clamp(adv_x2, min=0, max=1).detach()
 
         for _ in range(self.steps):
-            adv_images.requires_grad = True
-            outputs = self.get_logits(adv_images)
+            adv_x2.requires_grad = True
+            decision, score = self.get_logits(adv_x2)
 
             # Calculate loss
             if self.targeted:
-                cost = -loss(outputs, target_labels)
+                cost = -loss(score, target_y)
             else:
-                cost = loss(outputs, labels)
+                cost = loss(score, y)
 
-            # Update adversarial images
-            grad = torch.autograd.grad(cost, adv_images,
+            # Update adversarial x2
+            grad = torch.autograd.grad(cost, adv_x2,
                                        retain_graph=False, create_graph=False)[0]
 
-            adv_images = adv_images.detach() + self.alpha*grad.sign()
-            delta = torch.clamp(adv_images - images,
+            adv_x2 = adv_x2.detach() + self.alpha*grad.sign()
+            delta = torch.clamp(adv_x2 - x2,
                                 min=-self.eps, max=self.eps)
-            adv_images = torch.clamp(images + delta, min=0, max=1).detach()
+            adv_x2 = torch.clamp(x2 + delta, min=0, max=1).detach()
 
-        return adv_images
+        return adv_x2
