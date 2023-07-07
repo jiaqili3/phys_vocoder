1899fb1b4cb34928a6ae47e0001e15f3ba03c548
Jiaqi Li <120090727@link.cuhk.edu.cn>
2023-07-07 09:29:48 +0800

    debug XVEC1 model

--- attack/models/tdnn.py
+++ attack/models/tdnn.py
@@ -132,12 +132,62 @@ class XVEC(nn.Module):
 
     def encoding(self, x):
         return self.forward(x)[0]
+class XVEC1(nn.Module):
+    def __init__(self, nOut=512, p_dropout=0.1, log_input=True, **kwargs):
+        super(XVEC1, self).__init__()
+        print('Embedding size is %d.' % nOut)
+        self.torchfb = torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_fft=512, win_length=400, hop_length=160,
+                                                            window_fn=torch.hamming_window, n_mels=30)
+        self.log_input = log_input
+        self.instancenorm = nn.InstanceNorm1d(30)
 
-class XVEC1:
-    def __init__(self) -> None:
-        raise NotImplementedError
+        self.tdnn1 = nn.Conv1d(in_channels=30, out_channels=512, kernel_size=5, dilation=1)
+        self.bn_tdnn1 = nn.BatchNorm1d(512, momentum=0.1, affine=False)
+        self.dropout_tdnn1 = nn.Dropout(p=p_dropout)
 
+        self.tdnn2 = nn.Conv1d(in_channels=512, out_channels=512, kernel_size=5, dilation=2)
+        self.bn_tdnn2 = nn.BatchNorm1d(512, momentum=0.1, affine=False)
+        self.dropout_tdnn2 = nn.Dropout(p=p_dropout)
 
+        self.tdnn3 = nn.Conv1d(in_channels=512, out_channels=512, kernel_size=7, dilation=3)
+        self.bn_tdnn3 = nn.BatchNorm1d(512, momentum=0.1, affine=False)
+        self.dropout_tdnn3 = nn.Dropout(p=p_dropout)
+
+        self.tdnn4 = nn.Conv1d(in_channels=512, out_channels=512, kernel_size=1, dilation=1)
+        self.bn_tdnn4 = nn.BatchNorm1d(512, momentum=0.1, affine=False)
+        self.dropout_tdnn4 = nn.Dropout(p=p_dropout)
+
+        self.tdnn5 = nn.Conv1d(in_channels=512, out_channels=1500, kernel_size=1, dilation=1)
+        self.bn_tdnn5 = nn.BatchNorm1d(1500, momentum=0.1, affine=False)
+        self.dropout_tdnn5 = nn.Dropout(p=p_dropout)
+
+        self.fc1 = nn.Linear(3000, 512)
+        self.bn_fc1 = nn.BatchNorm1d(512, momentum=0.1, affine=False)
+        self.dropout_fc1 = nn.Dropout(p=p_dropout)
+
+        self.fc2 = nn.Linear(512, 512)
+        self.bn_fc2 = nn.BatchNorm1d(512, momentum=0.1, affine=False)
+        self.dropout_fc2 = nn.Dropout(p=p_dropout)
+
+        self.fc3 = nn.Linear(512, nOut)
+
+    def forward(self, x):
+        with torch.no_grad():
+            with torch.cuda.amp.autocast(enabled=False):
+                x = self.torchfb(x) + 1e-6
+                if self.log_input: x = x.log()
+                x = self.instancenorm(x)
+        x = self.dropout_tdnn1(self.bn_tdnn1(F.relu(self.tdnn1(x))))
+        x = self.dropout_tdnn2(self.bn_tdnn2(F.relu(self.tdnn2(x))))
+        x = self.dropout_tdnn3(self.bn_tdnn3(F.relu(self.tdnn3(x))))
+        x = self.dropout_tdnn4(self.bn_tdnn4(F.relu(self.tdnn4(x))))
+        x = self.dropout_tdnn5(self.bn_tdnn5(F.relu(self.tdnn5(x))))
+
+        stats = torch.cat((x.mean(dim=2), x.std(dim=2)), dim=1)
+        x = self.dropout_fc1(self.bn_fc1(F.relu(self.fc1(stats))))
+        x = self.dropout_fc2(self.bn_fc2(F.relu(self.fc2(x))))
+        x = self.fc3(x)
+        return x
 if __name__ == '__main__':
     model = XVEC(feat_dim=80, embed_dim=512, pooling_func='TSTP')
     model.eval()
