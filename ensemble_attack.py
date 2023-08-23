f6bfa3f763c9d7a242c18e3b5c187f12c1424e4d
Jiaqi Li <120090727@link.cuhk.edu.cn>
2023-08-23 10:48:37 +0800

    add ensemble file

---
+++ ensemble_attack.py
@@ -0,0 +1,140 @@
+from init_ensemble import config
+import torch
+import torchaudio
+from torch.utils.data import DataLoader
+import os
+import pdb
+import pandas
+from attack.attacks.pgd import PGD
+import logging
+from dataset.asvspoof2019 import ASVspoof2019
+
+device = 'cuda:0'
+
+model = config.model
+model = model.to(device)
+if config.phys_vocoder_model is not None:
+    config.phys_vocoder_model = config.phys_vocoder_model.to(device)
+
+
+adver_dir = config.attack.adv_dir
+adver_dir = os.path.join(adver_dir, f'{model.__class__.__name__}_{"None" if config.phys_vocoder_model is None else config.phys_vocoder_model.__class__.__name__}{"_Alt" if config.use_alternate_pipeline else ""}_{config.attack.steps}_{config.attack.alpha}_{config.attack.eps}')
+os.makedirs(adver_dir, exist_ok=True)
+logging.basicConfig(filename=f'{adver_dir}/log', encoding='utf-8', level=logging.DEBUG, force=True, format='%(asctime)-3s %(message)s')
+logging.info(f'adv samples saved to {adver_dir}')
+logging.info(f'use alt pipe: {config.use_alternate_pipeline}')
+logging.info(config.attack)
+
+class CombinedModel(torch.nn.Module):
+    def __init__(self, model, phys_vocoder):
+        super(CombinedModel, self).__init__()
+        self.phys_vocoder = phys_vocoder
+        self.model = model
+        if self.phys_vocoder is not None:
+            logging.info('using physical vocoder')
+    def forward(self, x1, x2):
+        # x1 enroll, x2 test
+        # return (decisions, cos sim)
+        if self.phys_vocoder is not None:
+            x2_voc = self.phys_vocoder(x2)
+        else:
+            x2_voc = x2
+        return self.model.make_decision_SV(x1, x2_voc)
+
+model.eval()
+if not config.use_alternate_pipeline:
+    combined_model = CombinedModel(model, config.phys_vocoder_model)
+else:
+    combined_model = CombinedModel(model, None)
+combined_model.eval()
+combined_model.threshold = model.threshold
+
+attacker = PGD(combined_model, steps=config.attack.steps, alpha=config.attack.alpha, random_start=False, eps=config.attack.eps)
+attacker.adver_dir = adver_dir
+
+# data
+dataset_name = 'ASVspoof2019'
+dataset_config = config.data[dataset_name].dataset
+dataset = ASVspoof2019(**dataset_config)
+dataloader = DataLoader(dataset, num_workers=4, batch_size=1, shuffle=False)
+logging.info('dataset: {}'.format(dataset_name))
+logging.info('dataset size: {}'.format(len(dataset)))
+
+def save_audio(advers, spk1_file_ids, spk2_file_ids, root, success, fs=16000):
+    result_file_path = os.path.join(root, 'attackResult.txt')
+    result_file = open(result_file_path, mode='a+')
+    for adver, spk1_file_id, spk2_file_id, suc in zip(advers[:, :], spk1_file_ids, spk2_file_ids, success):
+        file_name = '{}_{}'.format(spk1_file_id, spk2_file_id)
+        adver_path = os.path.join(root, file_name + ".wav")
+        adver = adver.detach().cpu()
+        adver = torch.unsqueeze(adver, 0)
+        logging.info(f'saved to {adver_path}')
+        # pdb.set_trace()
+        torchaudio.save(adver_path, adver, fs, encoding='PCM_S', bits_per_sample=16)
+        result_file.write('{} {}\n'.format(file_name, suc))
+    result_file.close()
+
+# attack
+success_cnt = 0
+total_cnt = 0
+
+with open('./audio_clipper/2500_list.txt') as flist:
+    flist = flist.readlines()
+
+df = {'steps': [], 'max_perturbation': [], 'success': []}
+
+for sample_no, item in enumerate(dataloader):
+    # shape: (batch_size, channels, audio_len)
+    x1 = item[config.data[dataset_name].waveform_index_spk1]
+    # to extract single channel?
+    x2 = item[config.data[dataset_name].waveform_index_spk2]
+    y = item[config.data[dataset_name].label_index]
+
+    # target attack & untarget attack
+    if int(y) == 1:
+        # only pick bona-fide audio
+        continue
+    x1, x2, y = x1.to(device), x2.to(device), y.to(device)
+
+    if config.use_alternate_pipeline:
+        # input passed through vocoder
+        assert config.phys_vocoder_model is not None
+        ori_x2 = x2.clone()
+        x2 = config.phys_vocoder_model(x2)
+
+    # speaker file id
+    spk1_file_ids = item[config.data[dataset_name].enroll_file_index]
+    spk2_file_ids = item[config.data[dataset_name].test_file_index]
+
+    flag = False
+    for spk1_file_id, spk2_file_id in zip(spk1_file_ids, spk2_file_ids):
+        # only the selected will be attacked
+        if f'{spk1_file_id}_{spk2_file_id}.wav\n' not in flist:
+            flag = True
+            break
+        file_name = '{}_{}'.format(spk1_file_id, spk2_file_id)
+        adver_path = os.path.join(adver_dir, file_name + ".wav")
+        # skip already existing ones
+        if os.path.exists(adver_path):
+            logging.info('adversarial audio already exists: {}'.format(adver_path))
+            flag = True
+            break
+    if flag:
+        continue
+
+    total_cnt += 1
+    adver, success, score = attacker(x1, x2, y, sample_no, df)
+    if config.use_alternate_pipeline:
+        adver = adver - x2 + ori_x2
+    logging.info(f'attack score: {score.item()}')
+
+    if len(adver.size()) == 3:
+        adver = adver.squeeze(1)
+    save_audio(advers=adver, spk1_file_ids=spk1_file_ids, spk2_file_ids=spk2_file_ids, root=adver_dir,success=success)
+    success_cnt += sum(success)
+    logging.info('success rate: {}/{}={}'.format(int(success_cnt), total_cnt, success_cnt / total_cnt))
+
+df = pandas.DataFrame(df)
+df.to_csv(os.path.join(adver_dir, 'attackResult.csv'), index=False)
+# remove f'{self.adver_dir}/a.wav'
+os.remove(f'{adver_dir}/a.wav')
---
+++ init_ensemble.py
@@ -0,0 +1,142 @@
+from easydict import EasyDict as edict
+import sys
+import torch
+
+config = edict()
+
+# use the alternate pipeline, which 
+# does not attack the vocoder model, only use it to gen input
+# when using the alternate pipeline, must have a physical vocoder 
+config.use_alternate_pipeline = False
+
+# ---------------------------------------- physical vocoder ---------------------------------------- #
+from phys_vocoder.hifigan.generator import HifiganEndToEnd
+from phys_vocoder.unet.unet import TestUNet,UNetMelLossFinetuned,UNetMelLossEndToEnd,UNetEndToEnd, UNetMixedLoss1NormalizedEndToEnd, UNetSpecLossEndToEnd, UNetSpecLossEndToEnd1, UNetMixedLossEndToEnd, UNetMixedLossNormalizedEndToEnd, UNetGAN, UNetGANNormalized
+
+# set to None if not using phys vocoder
+config.phys_vocoder_model = UNetMixedLossNormalizedEndToEnd
+
+# HifiGAN
+if config.phys_vocoder_model == HifiganEndToEnd:
+    config.phys_vocoder_model = HifiganEndToEnd()
+    config.phys_vocoder_model.load_model('/mntcephfs/lab_data/lijiaqi/hifigan-checkpoints/0630/model-115000.pt')
+
+# unet
+elif config.phys_vocoder_model == TestUNet:
+    config.phys_vocoder_model = TestUNet()
+    config.phys_vocoder_model.load_model('/home/lijiaqi/108427d78e3941708dce02e0dcd293a2/model-10416000.pt')
+    # /home/lijiaqi/108427d78e3941708dce02e0dcd293a2/model-40000.pt
+elif config.phys_vocoder_model == UNetEndToEnd:
+    config.phys_vocoder_model = UNetEndToEnd()
+    config.phys_vocoder_model.load_model('/mntcephfs/lab_data/lijiaqi/unet_checkpoints/0702/model-45000.pt')
+# unet trained with spec loss, A100 GPU
+elif config.phys_vocoder_model == UNetMelLossEndToEnd:
+    config.phys_vocoder_model = UNetMelLossEndToEnd()
+    config.phys_vocoder_model.load_model('/mntcephfs/lab_data/lijiaqi/unet_checkpoints/0812_mel_pretrain/model-6534000.pt')
+elif config.phys_vocoder_model == UNetMelLossFinetuned:
+    config.phys_vocoder_model = UNetMelLossFinetuned()
+    config.phys_vocoder_model.load_model('/home/lijiaqi/108427d78e3941708dce02e0dcd293a2/unet_checkpoints/model-221000.pt')
+elif config.phys_vocoder_model == UNetGAN:
+    config.phys_vocoder_model = UNetGAN()
+    config.phys_vocoder_model.load_model('/home/lijiaqi/108427d78e3941708dce02e0dcd293a2/model-7130000.pt')
+
+
+
+
+elif config.phys_vocoder_model == UNetSpecLossEndToEnd:
+    config.phys_vocoder_model = UNetSpecLossEndToEnd()
+    config.phys_vocoder_model.load_model('/mntcephfs/lab_data/lijiaqi/unet_checkpoints/0712_specloss/model-11840000.pt')
+elif config.phys_vocoder_model == UNetSpecLossEndToEnd1:
+    config.phys_vocoder_model = UNetSpecLossEndToEnd1()
+    config.phys_vocoder_model.load_model('/mntcephfs/lab_data/lijiaqi/unet_checkpoints/0710_specloss/model-88000.pt')
+elif config.phys_vocoder_model == UNetMixedLossEndToEnd:
+    config.phys_vocoder_model = UNetMixedLossEndToEnd()
+    config.phys_vocoder_model.load_model('/mntcephfs/lab_data/lijiaqi/unet_checkpoints/0717_mixedloss_0717recdata/model-33744000.pt')
+elif config.phys_vocoder_model == UNetMixedLossNormalizedEndToEnd:
+    config.phys_vocoder_model = UNetMixedLossNormalizedEndToEnd()
+    # config.phys_vocoder_model.load_model('/mntcephfs/lab_data/lijiaqi/unet_checkpoints/0719_mixedloss_normalized/model-3264000.pt')
+    config.phys_vocoder_model.load_model('/mntcephfs/lab_data/lijiaqi/unet_checkpoints/0719_mixedloss_normalized/model-4080000.pt')
+elif config.phys_vocoder_model == UNetMixedLoss1NormalizedEndToEnd:
+    config.phys_vocoder_model = UNetMixedLoss1NormalizedEndToEnd()
+    config.phys_vocoder_model.load_model('/mntcephfs/lab_data/lijiaqi/unet_checkpoints/0727_mixedloss1_normalized/model-1344000.pt')
+elif config.phys_vocoder_model == UNetGANNormalized:
+    config.phys_vocoder_model = UNetGANNormalized()
+    config.phys_vocoder_model.load_model('/home/lijiaqi/108427d78e3941708dce02e0dcd293a2/model-180000.pt')
+
+if config.phys_vocoder_model is None:
+    config.use_alternate_pipeline = False
+
+# ---------------------------------------- ASV model ---------------------------------------- #
+from attack.models.RawNet import RawNet3
+from attack.models.ECAPATDNN import ECAPATDNN
+from attack.models.ResNetSE34V2 import ResNetSE34V2
+from attack.models.tdnn import XVEC, XVEC1
+from attack.models.model_config import config as model_config
+
+config.model = [RawNet3, ECAPATDNN, ResNetSE34V2]
+for i in range(config.model):
+    if config.model[i] == RawNet3:
+        config.model[i] = RawNet3(**model_config['RawNet3'])
+        config.model[i].load_state_dict(torch.load('./pretrained_models/rawnet3.pt'))
+        config.model[i].threshold = 0.3295809328556061
+    elif config.model[i] == ECAPATDNN:
+        config.model[i] = ECAPATDNN(**model_config['ECAPATDNN'])
+        config.model[i].load_state_dict(torch.load('./pretrained_models/ECAPATDNN.pth'))
+        config.model[i].threshold = 0.33709782361984253
+    elif config.model[i] == ResNetSE34V2:
+        config.model[i] = ResNetSE34V2(**model_config['ResNetSE34V2'])
+        config.model[i].load_state_dict(torch.load('./pretrained_models/ResNetSE34V2.pth'))
+        config.model[i].threshold = 0.3702884316444397
+    elif config.model[i] == XVEC:
+        config.model[i] = XVEC(**model_config['XVEC'])
+        config.model[i].load_state_dict(torch.load('./pretrained_models/XVEC.pth'))
+        config.model[i].threshold = 0.879676103591919
+    elif config.model[i] == XVEC1:
+        config.model[i] = XVEC1()
+        config.model[i].load_state_dict(torch.load('./pretrained_models/XVEC1.pth'))
+        config.model[i].threshold = 0.28246
+
+# ---------------------------------------- Attack ---------------------------------------
+
+config.attack = edict()
+config.attack.adv_dir = '/mntcephfs/lab_data/lijiaqi/adver_out/'
+
+# steps: how many more times of attack to perform after a successful attack
+config.attack.steps = 0
+config.attack.alpha = 0.0004
+# config.attack.eps = 0.005
+config.attack.eps = 0.008
+
+# ---------------------------------------- dataset --------------------------------------- #
+config.data = edict()
+config.data.dataset_name = 'ASVspoof2019'
+
+# VoxCeleb1Verification
+# config.data.VoxCeleb1Verification = edict()
+# config.data.VoxCeleb1Verification.dataset = edict()
+# config.data.VoxCeleb1Verification.dataloader = edict()
+# config.data.VoxCeleb1Verification.dataset.root = '/mntcephfs/data/chenxi/datasets/VoxCeleb/VoxCeleb1'
+# config.data.VoxCeleb1Verification.dataset.meta_list_path = '/mntnfs/lee_data1/wangli/ASGSR/Vox1_10/veri_test_10_0.txt'
+# config.data.VoxCeleb1Verification.dataloader.batch_size = 1
+# config.data.VoxCeleb1Verification.waveform_index_spk1 = 0
+# config.data.VoxCeleb1Verification.waveform_index_spk2 = 1
+# config.data.VoxCeleb1Verification.label_index = 3
+# config.data.VoxCeleb1Verification.enroll_file_index = 4
+# config.data.VoxCeleb1Verification.test_file_index = 5  # spk1 for enroll, spk2 for test
+
+config.data.ASVspoof2019 = edict()
+config.data.ASVspoof2019.dataset = edict()
+config.data.ASVspoof2019.dataloader = edict()
+config.data.ASVspoof2019.dataset.data_file = './dataset/enroll_eval_pairs.txt'
+# config
+config.data.ASVspoof2019.dataset.train_path = '/mntnfs/lee_data1/wangli/ASVspoof2019/PA/ASVspoof2019_PA_train/flac'
+config.data.ASVspoof2019.dataset.dev_path = '/mntnfs/lee_data1/wangli/ASVspoof2019/PA/ASVspoof2019_PA_dev/flac'
+config.data.ASVspoof2019.dataset.eval_path = '/mntnfs/lee_data1/wangli/ASVspoof2019/PA/ASVspoof2019_PA_eval/flac'
+
+config.data.ASVspoof2019.dataloader.batch_size = 1
+config.data.ASVspoof2019.waveform_index_spk1 = 0
+config.data.ASVspoof2019.waveform_index_spk2 = 1
+config.data.ASVspoof2019.label_index = 3
+config.data.ASVspoof2019.enroll_file_index = 4
+config.data.ASVspoof2019.test_file_index = 5  # spk1 for enroll, spk2 for test
+
