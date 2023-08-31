feef2f2e6c17e2933ac4a922a5bfee0da35248f8
Jiaqi Li <120090727@link.cuhk.edu.cn>
2023-08-31 12:29:28 +0800

    update multiattack for joint

--- attack/attacks/multiattack.py
+++ attack/attacks/multiattack.py
@@ -51,23 +51,27 @@ class MultiAttack():
             pgd.adver_dir = self.adver_dir
             attackers.append(pgd)
         while True:
-            is_all_success = [True]*3
+            is_all_success = [True]*2
             for i in range(len(self.models)):
-                torchaudio.save(f'{self.adver_dir}/a.wav', adv_x2.cpu().detach().squeeze(0), 16000, encoding='PCM_S', bits_per_sample=16)
-                a = torchaudio.load(f'{self.adver_dir}/a.wav')[0].to(self.device).unsqueeze(0)
-
-                if self.models[i].forward(x1, a)[0].item() == 0:
-                    adv_x2, decision, _, sum_steps = attackers[i](x1, adv_x2, y)
-                    delta = torch.clamp(adv_x2 - x2,
-                                    min=-self.kwargs['eps'], max=self.kwargs['eps'])
-                    adv_x2 = torch.clamp(x2 + delta, min=-1, max=1).detach()
-                else:
-                    sum_steps = 0
+                adv_x2, decision, _, sum_steps = attackers[i](x1, adv_x2, y)
+                delta = torch.clamp(adv_x2 - x2,
+                                min=-self.kwargs['eps'], max=self.kwargs['eps'])
+                adv_x2 = torch.clamp(x2 + delta, min=-1, max=1).detach()
+                is_all_success[i] = decision
                 print(f'attack model {i} sum_steps: {sum_steps}')
-                if sum_steps != 0:
-                    is_all_success[i] = False
+            torchaudio.save(f'{self.adver_dir}/a.wav', adv_x2.cpu().detach().squeeze(0), 16000, encoding='PCM_S', bits_per_sample=16)
+            a = torchaudio.load(f'{self.adver_dir}/a.wav')[0].to(self.device).unsqueeze(0)
+            try:
+                decision1, score = self.models[0](x1, a)
+            except:
+                decision1, score = self.models[0].make_decision_SV(x1, a)
+
+            if decision1.item() == 0:
+                is_all_success[0] = False
+            else:
+                is_all_success[0] = True
             cnt += 1
-            if is_all_success == [True]*3:
+            if is_all_success == [True]*2:
                 if df is not None:
                     df['steps'].append(cnt)
                     print(f'sum_steps: {cnt}')
